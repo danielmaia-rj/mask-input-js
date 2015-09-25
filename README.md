15ab06048928f0af3e782b24d50674f46c21147f
Daniel Maia <maia.danielalmeida@gmail.com>
2015-09-25 12:42:06 -0300

    add files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mask-input-js 
+Mask Phone
+==========
+Javascript que formata campos do tipo telefone utilizando o plugin [jquery.mask](https://github.com/igorescobar/jQuery-Mask-Plugin), fazendo distinção entre os tipos de telefone fixo, celular e aplicando quando devido o nono dígito
+
+Usabilidade:
+------------
+
+    $(document).ready(function(){
+      $('input[name=phone]').maskphone();
+    });
---
+++ src/js/maskphone.js
@@ -0,0 +1,61 @@
+(function ($) {
+	"use strict";
+	$.fn.maskphone = function () {
+		var
+		elementoInput = this,
+		codigoAreaNonoDigito = [11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 24, 27, 28, 81, 82, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 96, 97, 98, 99],
+		fixoIniciadoCom = [2, 3, 4, 5],
+		celularIniciadoCom = [7, 8, 9],
+		mascaraAplicada = false;
+
+		init();
+
+		function init() {
+			aplicarMascaraInit(elementoInput);
+			$(elementoInput).on("keyup", function(){
+				aplicarMascaraOnKeyup($(this));
+			});
+		};
+
+		function aplicarMascaraInit(elemento) {
+			elemento.each(function(){
+
+				$(this).val($(this).val().replace(/\b0+/g,''));
+				aplicar(this);
+			});
+		};
+
+		function aplicarMascaraOnKeyup (elemento) {
+			elemento.each(function(){
+				$(this).val($(this).val().replace(/\b0+/g,''));
+				if ($(this).val().replace(/[^0-9\.]/g, '').length >= 3)
+					mascaraAplicada = false;
+
+				if ($(this).val().replace(/[^0-9\.]/g, '').length == 3 && mascaraAplicada == false) {
+					aplicar(this);
+				};
+			});
+		};
+
+		function aplicar(elemento) {
+			var codigoArea = $(elemento).val().replace(/[^0-9\.]/g, '').substring(0, 2);
+			var telefoneIniciadoCom = $(elemento).val().replace(/[^0-9\.]/g, '').substring(3,2);
+			var isTelefoneFixo = $.inArray(parseInt(telefoneIniciadoCom), fixoIniciadoCom) != -1;
+			var isTelefoneCelular = $.inArray(parseInt(telefoneIniciadoCom), celularIniciadoCom) != -1;
+			var isNonoDigito = $.inArray(parseInt(codigoArea), codigoAreaNonoDigito) != -1;
+
+			if(isTelefoneFixo) {
+				$(elemento).mask("(99)9999-9999");
+				mascaraAplicada = true;
+			} else if (isTelefoneCelular && isNonoDigito) {
+				$(elemento).mask("(99)99999-9999");
+				mascaraAplicada = true;
+			} else {
+				$(elemento).mask("(99)9999-9999");
+				mascaraAplicada = true;
+			};
+		};
+
+	};
+
+})(jQuery)
